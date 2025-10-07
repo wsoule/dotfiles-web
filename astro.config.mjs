d6c36d6832e2d8b522e837972cfe5e3e32b734e3
Wyat Soule <wsoule679@gmail.com>
2025-10-06 22:59:50 -0400

    remove md files & add node ssr for prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: node({ mode: "standalone" }),
   vite: {
     plugins: [tailwindcss()],
   },
